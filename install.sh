987aa2309c78023cdd7556ea8caea9e5c36e4391
pritomb <pritomb@bristlecone.cluster>
2025-03-26 18:58:42 -0700

    WIP

--- install.sh
+++ install.sh
@@ -19,6 +19,7 @@ if [ -d "$INSTALL_PATH" ]; then
 fi
 
 
+
 mkdir -p "$INSTALL_PATH"
 SOURCE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 rsync -av --delete --exclude='.git' "$SOURCE_DIR/" "$INSTALL_PATH/"
