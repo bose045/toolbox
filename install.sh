192af5ceeca098bafbc93876c0f1cfe5530ceb69
pritomb <pritomb@bristlecone.cluster>
2025-03-26 19:01:39 -0700

    WIP

--- install.sh
+++ install.sh
@@ -18,8 +18,6 @@ if [ -d "$INSTALL_PATH" ]; then
     echo "♻️  Overwriting existing toolbox..."
 fi
 
-
-
 mkdir -p "$INSTALL_PATH"
 SOURCE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 rsync -av --delete --exclude='.git' "$SOURCE_DIR/" "$INSTALL_PATH/"
